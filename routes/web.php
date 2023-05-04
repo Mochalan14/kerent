a5979e15900e6776a47752e5998fb6988c27dcbc
DevOnft <devantara69@gmail.com>
2023-05-04 15:26:39 +0700

    TampilanAdmin+
    penambahan web.php dan UserController.php

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,8 @@
 <?php
 
+use App\Http\Controllers\LoginController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
-
 
 /*
 |--------------------------------------------------------------------------
@@ -20,13 +20,20 @@ use Illuminate\Support\Facades\Route;
 // });
 
 Route::controller(UserController::class)->group(function () {
-    Route::get('/', 'index');
-    Route::get('/sewaku', 'sewaku')->name('sewaku');
+    Route::get('/', 'index')->name('user.index');
+    Route::get('/sewaku', 'sewaku')->name('sewaku')->middleware('auth');
+    Route::get('/sewamobil', 'buatSewa')->middleware('auth');
+    Route::post('/prosesSewa', 'prosesSewa');
+});
 
+Route::controller(LoginController::class)->group(function () {
+    Route::get('/login', 'login')->name('login');
+    Route::get('/register', 'register');
+    Route::get('/logout', 'logout');
+    Route::post('/registerproses', 'registerproses');
+    Route::post('/proseslogin', 'proseslogin');
 });
 
 Route::get('/admin', function () {
     return view('admin.index');
 });
-
-
